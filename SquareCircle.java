1d639ecbab1a95eb03bb0efc15c954b36d1b804a
VladimirLadnov <volodimir1ladnov@gmail.com>
2016-04-16 17:23:57 +0300

    Square of Circle

---
+++ SquareCircle.java
@@ -0,0 +1,11 @@
+package dz;
+
+/**
+ * Created by Володимир on 14.04.2016.
+ */
+public class SquareCircle {
+    public static void main(String[] args) {
+        Circle square = new Circle(4);
+        System.out.println("Square of circle: " + square.square());
+    }
+}
